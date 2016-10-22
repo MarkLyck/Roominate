d50aa84e4a3292d999393ffe8ee70d7753ef7b40
Mark Lyck <mark.lyck@gmail.com>
2016-10-21 21:38:28 -0500

    Ejected, Added PostCSS, Scss functionality and installed react-router v4

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Goodbye Blue Monday</h2>
         </div>
+        <button className="button is-primary">Test</button>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
