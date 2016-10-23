bb3633cbe9280f35f406b3df40bb7df940882863
Mark Lyck <mark.lyck@gmail.com>
2016-10-22 23:35:28 -0500

    Changed contact button to an email link

--- src/containers/User/Profile.jsx
+++ src/containers/User/Profile.jsx
@@ -12,43 +12,46 @@ class Profile extends React.Component {
     }
 
   render() {
-    const session = this.state.session;
+    let user = this.state.session;
+    if(this.props.pathname !== '/profile') {
+      user = this.state.session;
+    }
     return (
       <div className="profile-page">
 
-        <div className="profile-img" style={{backgroundImage: `url(${session.image_url})`}}/>
-        <h1 className="name">{session.first_name} {session.last_name}</h1>
+        <div className="profile-img" style={{backgroundImage: `url(${user.image_url})`}}/>
+        <h1 className="name">{user.first_name} {user.last_name}</h1>
 
         <section className="content">
           <ul className="info-list">
             <li className="info-item">
-              <h4>Gender:</h4><h4>{session.sex}</h4>
+              <h4>Gender:</h4><h4>{user.sex}</h4>
             </li>
             <li className="info-item">
-              <h4>Age:</h4><h4>{session.age}</h4>
+              <h4>Age:</h4><h4>{user.age}</h4>
             </li>
             <li className="info-item">
-              <h4>Smoker:</h4><h4>{session.is_smoker ? 'yes' : 'no'}</h4>
+              <h4>Smoker:</h4><h4>{user.is_smoker ? 'yes' : 'no'}</h4>
             </li>
             <li className="info-item">
-              <h4>Has Pets:</h4><h4>{session.has_pets ? 'yes' : 'no'}</h4>
+              <h4>Has Pets:</h4><h4>{user.has_pets ? 'yes' : 'no'}</h4>
             </li>
             <li className="info-item">
-              <h4>Has Children:</h4><h4>{session.has_children ? 'yes' : 'no'}</h4>
+              <h4>Has Children:</h4><h4>{user.has_children ? 'yes' : 'no'}</h4>
             </li>
             <li className="info-item">
-              <h4>LGBTQ Friendly:</h4><h4>{session.lgbtq_friendly ? 'yes' : 'no'}</h4>
+              <h4>LGBTQ Friendly:</h4><h4>{user.lgbtq_friendly ? 'yes' : 'no'}</h4>
             </li>
             <li className="info-item">
-              <h4>Relationship Status:</h4><h4>{session.relationship_status}</h4>
+              <h4>Relationship Status:</h4><h4>{user.relationship_status}</h4>
             </li>
             <li className="info-item">
-              <h4>Temperament</h4><h4>{session.temperament}</h4>
+              <h4>Temperament</h4><h4>{user.temperament}</h4>
             </li>
           </ul>
         </section>
 
-        <button className="button is-primary contact">Contact {session.first_name}</button>
+        <a className="button is-primary contact" href={`mailto:${user.email}`}>Contact {user.first_name}</a>
       </div>
     );
   }
